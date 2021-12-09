1310c3d4a46aa9bd18e9a258795265a5ef8ce848
MASSE Estelle <masseestelle@gmail.com>
2021-12-09 23:18:35 +0100

    Update modele_donnees.php

--- P4/v2/P4_02_schemasUML/modele_donnees.php
+++ P4/v2/P4_02_schemasUML/modele_donnees.php
@@ -3,161 +3,175 @@
 // Création de la table - ADRESSE
 //-----------------------------------------------------
 CREATE TABLE adresse (
-	adresse_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	adresse_num_voie INT
-	adresse_type_voie ENUM('Rue', 'Allée', 'Avenue', 'Boulevard', 'Impasse', 'Place', 'Route', 'Carrefour', 'Chemin', 'Chaussée', 'Cité', 'Corniche', 'Cours', 'Domaine', 'Descente', 'Ecart', 'Esplanade', 'Faubourg', 'Grande Rue', 'Hameau', 'Halle', 'Lieu-dit', 'Lotissement', 'Marché', 'Montée', 'Passage', 'Plaine', 'Plateau', 'Promenade', 'Parvis', 'Quartier', 'Quai', 'Résidence', 'Ruelle', 'Rocade', 'Rond-point', 'Sente - Sentier', 'Square', 'Terre-plein', 'Traverse', 'Villa', 'Village')
-	adresse_nom_voie VARCHAR(60)
-	adresse_complement VARCHAR(60)
-	adresse_code_postal VARCHAR(5)
-	adresse_ville VARCHAR(20)
-	adresse_added_date DATETIME
-	adresse_update_date DATETIME
-	adresse_deleted_date DATETIME
-)
+	adresse_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	adresse_num_voie INT UNSIGNED NOT NULL,
+	adresse_type_voie ENUM('Rue', 'Allée', 'Avenue', 'Boulevard', 'Impasse', 'Place', 'Route', 'Carrefour', 'Chemin', 'Chaussée', 'Cité', 'Corniche', 'Cours', 'Domaine', 'Descente', 'Ecart', 'Esplanade', 'Faubourg', 'Grande Rue', 'Hameau', 'Halle', 'Lieu-dit', 'Lotissement', 'Marché', 'Montée', 'Passage', 'Plaine', 'Plateau', 'Promenade', 'Parvis', 'Quartier', 'Quai', 'Résidence', 'Ruelle', 'Rocade', 'Rond-point', 'Sente - Sentier', 'Square', 'Terre-plein', 'Traverse', 'Villa', 'Village') NOT NULL DEFAULT 'Rue',
+	adresse_nom_voie VARCHAR(60) NOT NULL,
+	adresse_complement VARCHAR(60) NULL,
+	adresse_code_postal VARCHAR(5) NOT NULL,
+	adresse_ville VARCHAR(20) NOT NULL,
+	adresse_added_date DATETIME NOT NULL,
+	adresse_update_date DATETIME NULL,
+	adresse_deleted_date DATETIME NULL
+) ENGINE=InnoDB;
 
 
 //-----------------------------------------------------
 // Création de la table - MEDIA
 //-----------------------------------------------------
 CREATE TABLE media (
-	media_id INT
-	media_type ENUM('.gif', '.jpg', '.jpeg', '.jfif', '.pjpeg', '.pjp', '.png')
-	media_nom VARCHAR(30)
-	media_chemin VARCHAR(100)
-	media_added_date DATETIME
-	media_added_deleted DATETIME
-)
+	media_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	media_type ENUM('.gif', '.jpg', '.jpeg', '.jfif', '.pjpeg', '.pjp', '.png') NOT NULL,
+	media_nom VARCHAR(30) NOT NULL UNIQUE,
+	media_chemin VARCHAR(100) NOT NULL UNIQUE,
+	media_added_date DATETIME NOT NULL,
+	media_deleted_date DATETIME NULL
+) ENGINE=InnoDB;
 
 
 //-----------------------------------------------------
 // Création de la table - CLIENT
 //-----------------------------------------------------
 CREATE TABLE client (
-	client_id INT
-	client_nom VARCHAR(30)
-	client_prenom VARCHAR(30)
-	client_pseudo VARCHAR(30)
-	client_mail VARCHAR(50)
-	client_telephone_fr_fr VARCHAR(10)
-	client_domicile INT
-	client_bureau INT
-	client_hash_mdp VARCHAR(100)
-	client_token VARCHAR(100)
-	client_added_date DATETIME
-	client_update_date DATETIME
-	client_deleted_date DATETIME
-)
+	client_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	client_nom VARCHAR(30) NOT NULL,
+	client_prenom VARCHAR(30) NOT NULL,
+	client_pseudo VARCHAR(30) NOT NULL UNIQUE,
+	client_mail VARCHAR(50) NOT NULL UNIQUE,
+	client_telephone_fr_fr VARCHAR(10) NOT NULL UNIQUE,
+	fk_client_domicile INT UNSIGNED NOT NULL,
+	fk_client_bureau INT UNSIGNED NULL,
+	client_hash_mdp VARCHAR(100) NOT NULL,
+	client_token VARCHAR(100) NULL UNIQUE,
+	client_added_date DATETIME NOT NULL,
+	client_update_date DATETIME NULL,
+	client_deleted_date DATETIME NULL,
+		//FOREIGN KEY(fk_client_domicile) REFERENCES adresse(id) ON DELETE CASCADE,
+		//FOREIGN KEY(fk_client_bureau) REFERENCES adresse(id) ON DELETE CASCADE,
+		CONSTRAINT uc_client UNIQUE (client_nom, client_prenom)
+) ENGINE=InnoDB;
 
 
 //-----------------------------------------------------
 // Création de la table - RESTAURATEUR
 //-----------------------------------------------------
 CREATE TABLE restaurateur (
-	restaurateur_id INT
-	restaurateur_nom VARCHAR(30)
-	restaurateur_prenom VARCHAR(30)
-	restaurateur_nom_restaurant VARCHAR(50)
-	restaurateur_mail VARCHAR(50)
-	restaurateur_telephone_fr_fr VARCHAR(10)
-	restaurateur_adresse INT
-	restaurateur_hash_mdp VARCHAR(100)
-	restaurateur_token VARCHAR(100)
-	restaurateur_added_date DATETIME
-	restaurateur_update_date DATETIME
-	restaurateur_deleted_date DATETIME
-)
+	restaurateur_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	restaurateur_nom VARCHAR(30) NOT NULL,
+	restaurateur_prenom VARCHAR(30) NOT NULL,
+	restaurateur_nom_restaurant VARCHAR(50) NOT NULL UNIQUE,
+	restaurateur_mail VARCHAR(50) NOT NULL UNIQUE,
+	restaurateur_telephone_fr_fr VARCHAR(10) NOT NULL UNIQUE,
+	fk_restaurateur_adresse INT UNSIGNED NOT NULL,
+	restaurateur_hash_mdp VARCHAR(100) NOT NULL,
+	restaurateur_token VARCHAR(100) NULL UNIQUE,
+	restaurateur_added_date DATETIME NOT NULL,
+	restaurateur_update_date DATETIME NULL,
+	restaurateur_deleted_date DATETIME NULL,
+		//FOREIGN KEY(fk_restaurateur_adresse) REFERENCES adresse(id),
+		CONSTRAINT uc_restaurateur UNIQUE (restaurateur_nom, restaurateur_prenom)
+) ENGINE=InnoDB;
 
 
 //-----------------------------------------------------
 // Création de la table - LIVREUR
 //-----------------------------------------------------
 CREATE TABLE livreur (
-	livreur_id INT
-	livreur_nom VARCHAR
-	livreur_prenom VARCHAR
-	livreur_pseudo VARCHAR
-	livreur_mail VARCHAR
-	livreur_telephone VARCHAR
-	livreur_latitude DECIMAL
-	livreur_longitude DECIMAL
-	livreur_statut ENUM
-	livreur_added_date DATETIME
-	livreur_deleted_date DATETIME
-)
+	livreur_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	livreur_nom VARCHAR(30) NOT NULL,
+	livreur_prenom VARCHAR(30) NOT NULL,
+	livreur_pseudo VARCHAR(30) NOT NULL UNIQUE,
+	livreur_mail VARCHAR(50) NOT NULL UNIQUE,
+	livreur_telephone_fr_fr VARCHAR(10) NOT NULL UNIQUE,
+	livreur_latitude DECIMAL NULL,
+	livreur_longitude DECIMAL NULL,
+	livreur_statut ENUM('inactif', 'actif', 'en livraison') NOT NULL DEFAULT 'inactif',
+	livreur_added_date DATETIME NOT NULL,
+	livreur_update_date DATETIME NULL,
+	livreur_deleted_date DATETIME NULL,
+		CONSTRAINT uc_livreur UNIQUE (livreur_nom, livreur_prenom)
+) ENGINE=InnoDB;
 
 
 //-----------------------------------------------------
 // Création de la table - PLAT
 //-----------------------------------------------------
 CREATE TABLE plat (
-	plat_id INT
-	plat_nom VARCHAR
-	plat_ingrédient 
-	plat_media
-	plat_prix
-	plat_type
-	plat_statut
-	plat_restaurateur
-	plat_added_date
-	plat_update_date
-	plat_deleted_date
-)
+	plat_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	plat_nom VARCHAR(60) NOT NULL UNIQUE,
+	plat_ingrédient TEXT NULL,
+	fk_plat_media INT UNSIGNED NULL,
+	plat_prix DECIMAL UNSIGNED NOT NULL,
+	plat_type ENUM('plat', 'dessert') NOT NULL,
+	plat_statut BOOLEAN NOT NULL DEFAULT '0',
+	fk_plat_restaurateur INT UNSIGNED NOT NULL,
+	plat_added_date DATETIME NOT NULL,
+	plat_update_date DATETIME NULL,
+	plat_deleted_date DATETIME NULL,
+		//FOREIGN KEY(fk_plat_media) REFERENCES media(id),
+		//FOREIGN KEY(fk_plat_restaurateur) REFERENCES restaurateur(id)
+) ENGINE=InnoDB;
 
 
 //-----------------------------------------------------
 // Création de la table - MENU
 //-----------------------------------------------------
 CREATE TABLE menu (
-	menu_id
-	//menu_restaurateur
-	menu_plat
-	menu_quantité_plat
-	menu_added_date
-	menu_update_date
-	menu_deleted_date
-)
+	menu_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	fk_menu_restaurateur INT UNSIGNED NOT NULL,
+	fk_menu_plat INT UNSIGNED NOT NULL,
+	menu_quantité_plat INT UNSIGNED NOT NULL,
+	menu_added_date DATETIME NOT NULL,
+	menu_update_date DATETIME NULL,
+	menu_deleted_date DATETIME NULL,
+		//FOREIGN KEY(fk_menu_restaurateur) REFERENCES restaurateur(id),
+		//FOREIGN KEY(fk_menu_plat) REFERENCES plat(id)
+) ENGINE=InnoDB;
 
 
 //-----------------------------------------------------
 // Création de la table - PANIER
 //-----------------------------------------------------
 CREATE TABLE panier (
-	panier_id
-	panier_client
-	panier_plat
-	panier_quantite_plat
-	panier_added_date
-	panier_deleted_date
-)
+	panier_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	fk_panier_client INT UNSIGNED NOT NULL,
+	fk_panier_plat INT UNSIGNED NOT NULL,
+	panier_quantite_plat INT UNSIGNED NOT NULL,
+	panier_added_date DATETIME NOT NULL,
+	panier_deleted_date DATETIME NULL,
+		//FOREIGN KEY(fk_panier_client) REFERENCES client(id),
+		//FOREIGN KEY(fk_panier_plat) REFERENCES plat(id)
+) ENGINE=InnoDB;
 
 
 //-----------------------------------------------------
 // Création de la table - COMMANDE
 //-----------------------------------------------------
 CREATE TABLE commande (
-	commande_id
-	commande_panier
-	commande_prix
-	commande_livreur
-	commande_temps_livraison
-	commande_etat
-	//commande_notation
-	//commande_commentaire
-	commande_added_date
-	commande_deleted_date
-)
+	commande_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	fk_commande_panier INT UNSIGNED NOT NULL,
+	commande_prix DECIMAL UNSIGNED NOT NULL,
+	fk_commande_livreur INT UNSIGNED NOT NULL,
+	commande_temps_livraison TIME NOT NULL,
+	commande_added_date DATETIME NOT NULL,
+	commande_deleted_date DATETIME NULL,
+		//FOREIGN KEY(fk_commande_panier) REFERENCES panier(id),
+		//FOREIGN KEY(fk_commande_livreur) REFERENCES livreur(id)
+) ENGINE=InnoDB;
 
 
 //-----------------------------------------------------
 // Création de la table - STOCK
 //-----------------------------------------------------
 CREATE TABLE stock (
-	stock_id
-	stock_plat
-	stock_livreur
-	stock_quantité
-	stock_added_date
-	stock_update_date
-	stock_deleted_date
-)
+	stock_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	fk_stock_plat INT UNSIGNED NOT NULL,
+	fk_stock_livreur INT UNSIGNED NOT NULL,
+	stock_quantité INT UNSIGNED NOT NULL,
+	stock_added_date DATETIME NOT NULL,
+	stock_update_date DATETIME NULL,
+	stock_deleted_date DATETIME NULL,
+		//FOREIGN KEY(fk_stock_plat) REFERENCES plat(id),
+		//FOREIGN KEY(fk_stock_livreur) REFERENCES livreur(id)
+) ENGINE=InnoDB;
 ?>
